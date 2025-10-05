e6f3c2091a976a0407d18674956b289700fc9e3b
Vansh Madaan <madaanvansh68@gmail.com>
2025-10-05 15:23:38 +0530

    Fix CORS for new frontend https://helpdeskmini2.final2

--- backend/app.js
+++ backend/app.js
@@ -127,7 +127,7 @@ const app = express();
 // Allowed frontends
 const allowedOrigins = [
   'https://helpdsk.netlify.app',
-  'https://helpdeskapp2.netlify.app'
+  'https://help-desk-mini-app.netlify.app'
 ];
 
 // Middlewares
